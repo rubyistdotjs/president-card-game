2052eef769f8377f3ed6a1e089cec50b52902cca
rubyistdotjs <rubyistdotjs@gmail.com>
2019-12-22 17:02:36 +0100

    Add Public Sans

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,30 @@
 module.exports = {
   theme: {
     extend: {
+      fontFamily: {
+        sans: [
+          'Public Sans',
+          '-apple-system',
+          'BlinkMacSystemFont',
+          '"Segoe UI"',
+          'Roboto',
+          '"Helvetica Neue"',
+          'Arial',
+          '"Noto Sans"',
+          'sans-serif',
+          '"Apple Color Emoji"',
+          '"Segoe UI Emoji"',
+          '"Segoe UI Symbol"',
+          '"Noto Color Emoji"',
+        ],
+      },
       cursor: {
         grab: 'grab',
       },
     },
   },
-  variants: {},
+  variants: {
+    margin: ['responsive', 'hover'],
+  },
   plugins: [],
 };
